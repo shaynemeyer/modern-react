d9d43c809b59beb0b66b3df7edc98218accccfe4
Shayne Meyer <shayne.meyer@gmail.com>
2017-01-18 12:41:08 -0800

    added posts new to create a post

---
+++ ReactBlog/src/components/PostsNew.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
+
+class PostsNew extends Component {
+  render(){
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create a New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
+        </div>
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" {...content} />
+        </div>
+
+        <button className="btn btn-primary" type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+// connect: 1st arg is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
+export default reduxForm({
+  form: 'PostNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
--- ReactBlog/src/routes.js
+++ ReactBlog/src/routes.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-import Posts from './components/Posts';
+import PostsIndex from './components/PostsIndex';
+import PostsNew from './components/PostsNew';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute components={Posts}/>
-
+    <IndexRoute components={PostsIndex}/>
+    <Route path="posts/new" component={PostsNew} />
   </Route>
 );
